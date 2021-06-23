0e391db1bfe4d29d704c45ea25f44ca9da60f990
Murugan Nagarajan <murugan425@gmail.com>
2021-06-24 00:07:22 +0530

    Add a separate module file to keep track or import all the required material components.

---
+++ src/app/core/modules/material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  imports: [
+  ],
+  exports: [  
+  ]
+})
+
+//To keep track of all the required Material Modules required.
+export class MaterialModule { }
