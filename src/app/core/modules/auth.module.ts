62365f970fd4f190d6a7af642d2338d8a02c00a4
Murugan Nagarajan <murugan425@gmail.com>
2021-06-26 02:21:00 +0530

    Add the routes related to Auth Module using a separate module class and import it inside AuthModule.

--- src/app/core/modules/auth.module.ts
+++ src/app/core/modules/auth.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { SigninComponent } from 'src/app/auth/signin/signin.component';
 import { SignupComponent } from 'src/app/auth/signup/signup.component';
+import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
     declarations: [
         SigninComponent,
         SignupComponent
+    ],
+    imports: [
+        AuthRoutingModule
     ]
 })
 
