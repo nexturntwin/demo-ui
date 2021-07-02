a7ab292df4d33008b5289d2ff252cfd3de55cae6
Murugan Nagarajan <murugan425@gmail.com>
2021-07-03 00:20:53 +0530

    Added the SideNavigation to the layout.

--- src/app/core/modules/material.module.ts
+++ src/app/core/modules/material.module.ts
@@ -8,6 +8,8 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 const materialModules = [
   MatButtonModule,
@@ -18,7 +20,9 @@ const materialModules = [
   MatSlideToggleModule,
   MatToolbarModule,
   MatIconModule,
-  MatMenuModule
+  MatMenuModule,
+  MatSidenavModule,
+  MatListModule
 ]
 
 @NgModule({
--- src/app/feature/common/home/home.component.ts
+++ src/app/feature/common/home/home.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  currentUser: String;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.currentUser = "Murugan Nagarajan"
   }
 
 }
